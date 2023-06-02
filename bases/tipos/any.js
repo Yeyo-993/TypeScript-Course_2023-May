d567bc6cf547cbb1224baf5e4fa148f6751591c4
Sergio Reinoso <slrs993@gmail.com>
2023-06-01 22:39:25 -0500

    Tipo any

---
+++ bases/tipos/any.js
@@ -0,0 +1,12 @@
+"use strict";
+(() => {
+    let avenger;
+    let exists;
+    let power;
+    avenger = 'Dr. Strnge';
+    console.log(avenger.charAt(0));
+    avenger = 150.654654;
+    console.log(avenger.toFixed(2));
+    console.log(exists);
+    console.log(power);
+})();
