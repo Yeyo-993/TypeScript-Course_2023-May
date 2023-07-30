2940962861be45c3a704607484f3d440cc46a2de
Sergio Reinoso <slrs993@gmail.com>
2023-07-30 18:01:40 -0500

    Parte de las bases terminado

--- bases/main.js
+++ bases/main.js
@@ -1,61 +1,12 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Alen',
-        age: 24,
-        powers: [1, 2]
+var Validations;
+(function (Validations) {
+    Validations.validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        age: 24,
-        powers: [1],
-        getName() {
-            return this.name;
-        }
+    Validations.validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-})();
-(() => {
-    class Mutant {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return this.name + ' ' + this.realName;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'Fernando',
-        age: 25,
-        address: {
-            id: 125,
-            zip: 'afkjas',
-            city: 'Otawa'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-    const client2 = {
-        name: 'Melissa',
-        age: 30,
-        address: {
-            id: 120,
-            city: 'Toronto',
-            zip: 'K2S SUN'
-        },
-        getFullAddress(id) {
-            return this.address.city;
-        }
-    };
-})();
-(() => {
-    let addNumbersFunction;
-    addNumbersFunction = (a) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
+console.log(Validations.validateText('Ser'));
 //# sourceMappingURL=main.js.map
